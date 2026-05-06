9df3bf5ab3a464e43bdb65146d4db33a93347ac4
Mikael Lindqvist <li.mikael@gmail.com>
2026-05-06 22:03:37 +0800

    can actually kind of working

--- examples/cantest/cantest.js
+++ examples/cantest/cantest.js
@@ -2,10 +2,24 @@ initEspBus(5,4);
 
 setInterval(()=>{
 	//console.log("writing...");
-	getBus().writeSlcan("t12320824")
+	//getBus().writeSlcan("t12320824")
 	//console.log("wrote...");
 },1000);
 
 getBus().on("slcan",s=>{
-	console.log(s);
+//	console.log(s);
 });
+
+//gc();
+
+globalThis.m=new MasterDevice(getBus());
+globalThis.d=globalThis.m.createRemoteDevice(5);
+let d=globalThis.d;
+
+d.insert(0x2001,1);
+d.insert(0x6201,1);
+d.at(0x2001,1).setInt(1);
+
+setInterval(()=>{
+	d.at(0x6201,1).setInt(!d.at(0x6201,1).getInt());
+},1000);
